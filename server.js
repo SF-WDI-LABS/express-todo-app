f97fdae86796eb55ca6a6c6897e4bd36b1321ef6
adamrobertreid <WDI_GA@Adams-MacBook-Pro-3.local>
2016-03-29 20:32:10 -0700

    two complete, still very lost

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.use(express.static(__dirname + '/public'));
 
 // our database is an array for now with some hardcoded values
 var todos = [
-  // { _id: 1, task: 'Laundry', description: 'Wash clothes' },
-  // { _id: 2, task: 'Grocery Shopping', description: 'Buy dinner for this week' },
-  // { _id: 3, task: 'Homework', description: 'Make this app super awesome!' }
+  { _id: 1, task: 'Laundry', description: 'Wash clothes' },
+  { _id: 2, task: 'Grocery Shopping', description: 'Buy dinner for this week' },
+  { _id: 3, task: 'Homework', description: 'Make this app super awesome!' }
 ];
 
 /**********
@@ -50,18 +50,30 @@ app.get('/api/todos/search', function search(req, res) {
 });
 
 app.get('/api/todos', function index(req, res) {
-  /* This endpoint responds with all of the todos
+res.json(200,{todos:todos});  /* This endpoint responds with all of the todos
    */
 });
 
 app.post('/api/todos', function create(req, res) {
-  /* This endpoint will add a todo to our "database"
+
+   /* This endpoint will add a todo to our "database"
    * and respond with the newly created todo.
    */
+   var newTask = req.body.task; //
+   var newDescription = req.body.description;
+   var newId = todos.length+1;
+   var newTaskObject = {_id: newId, task: newTask, Description: newDescription};
+   todos.push(newTaskObject);
+   res.status(200).json(newTaskObject);
 });
 
 app.get('/api/todos/:id', function show(req, res) {
-  /* This endpoint will return a single todo with the
+  var id = parseInt(req.params.id);
+   console.log(id);
+   var theRightOne;
+   return todo._id == id;})[0]);
+   res.json(200)({});
+   /* This endpoint will return a single todo with the
    * id specified in the route parameter (:id)
    */
 });
